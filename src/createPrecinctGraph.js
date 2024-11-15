5eca0fb792b2d7a7609d2176f33b656ad15f982e
jacquesdaytona <osistlk@gmail.com>
2024-11-15 13:34:13 -0500

    Create precinct graph visualization and output to DOT format

--- src/createPrecinctGraph.js
+++ src/createPrecinctGraph.js
@@ -1,13 +1,27 @@
-// Parse ballot data from the JSON file
-// Create graph nodes for each precinct
-// Create graph edges between neighboring precincts
-// Output the graph data in a JSON file
-
+// read graph json file and create precinct graphviz file
 const fs = require('fs');
 
-// Define the path to the JSON file
-const filepath = 'data/ballots_by_precinct.json';
+const precinctGraph = JSON.parse(fs.readFileSync('data/precinct_graph.json'));
 
-// Read the JSON file synchronously and parse its content
-const file = fs.readFileSync(filepath, 'utf8');
-const data = JSON.parse(file);
+// node class
+// id: precinct id
+// name: precinct name
+// district: district id
+// neighbors: array of neighbor nodes by precinct id
+
+// create digraph string for precinct graph
+let digraph = 'digraph precinct_graph {\n';
+digraph += 'node [shape=ellipse];\n';
+digraph += 'edge [color=black];\n';
+
+for (const precinct of precinctGraph) {
+    digraph += `${precinct.id} [label="${precinct.name}"];\n`;
+    for (const neighbor of precinct.neighbors) {
+        digraph += `${precinct.id} -> ${neighbor};\n`;
+    }
+}
+
+digraph += '}\n';
+
+fs.writeFileSync('data/precinct_graph.dot', digraph);
+console.log('precinct graph created');
