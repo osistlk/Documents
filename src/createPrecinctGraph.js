c6f82e94e078b821b92a653a2049d9e35a54180a
jacquesdaytona <osistlk@gmail.com>
2024-11-16 12:59:36 -0500

    Add smoothing and rotation options to precinct graph; remove unused flip scripts

--- src/createPrecinctGraph.js
+++ src/createPrecinctGraph.js
@@ -10,6 +10,8 @@ function createPrecinctGraph() {
     graph += '    node [shape=circle, style=filled, color=darkblue, fontname="Arial"];\n';
     graph += '    edge [color="#666666"];\n';
     graph += '    bgcolor="#FFFFED";\n';
+    graph += '    smoothing=triangle;\n';
+    graph += '    rotate=0;\n';
 
     let precincts = JSON.parse(fs.readFileSync('data/raw_precinct_graph.json'));
     const metadata = JSON.parse(fs.readFileSync('data/ballots_by_precinct.json'));
