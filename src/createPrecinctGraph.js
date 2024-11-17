5332b0a2e125af71b7c2f08f158a6c7b38ca5964
jacquesdaytona <osistlk@gmail.com>
2024-11-16 21:20:04 -0500

    Remove minimum votes calculation and update precinct graph node label formatting for improved clarity

--- src/createPrecinctGraph.js
+++ src/createPrecinctGraph.js
@@ -102,7 +102,6 @@ function createPrecinctGraph() {
     }
 
     const maxVotes = Math.max(...precincts.filter(p => p.totalVotes > 0).map(p => p.totalVotes));
-    const minVotes = Math.min(...precincts.filter(p => p.totalVotes > 0).map(p => p.totalVotes));
     const meanVotes = precincts.reduce((acc, cur) => acc + cur.totalVotes, 0) / precincts.length;
     // set precinct 700 to the mean votes
     precincts.find(p => p.id === 700).totalVotes = meanVotes.toFixed(0);
@@ -119,7 +118,7 @@ function createPrecinctGraph() {
             const textColor = notBlueValue < 0x8888 ? 'white' : 'black';
             // normalize precinct size by total votes between 0.1 and 1.0
             const size = Math.max(0.1, Math.min(1.0, precinct.totalVotes / maxVotes)).toFixed(2);
-            graph += `    ${precinct.id} [shape = circle; style = filled;label="${precinct.id}\\n${precinct.name}", fillcolor="${fillColor}", fontcolor="${textColor}", width="${size}", height="${size}", color="${precinct.heat || 'gray10'}"];\n`;
+            graph += `    ${precinct.id} [shape = circle; style = filled;label="", fillcolor="${fillColor}", fontcolor="${textColor}", width="${size}", height="${size}", color="${precinct.heat || 'gray10'}"];\n`;
             if (precinct.neighbors.length > 0) {
                 graph += `    ${precinct.id} -- {${precinct.neighbors.join(',')}};\n`;
             }
