4ae184ebd14ed5c6d1a266ac32aae85b52f80e99
jacquesdaytona <osistlk@gmail.com>
2024-11-18 13:01:53 -0500

    Parse election data and save results to parsed.json

--- gis/fairfax/parse.js
+++ gis/fairfax/parse.js
@@ -1,3 +1,16 @@
 const fs = require('fs');
 
 const data = JSON.parse(fs.readFileSync('gis/fairfax/data.json', 'utf8'));
+
+const parsedData = data.breakdownResults.map((result) => {
+    const name = result.precinct.name[0].text;
+    const id = name.split('-')[0].trim();
+    const totalVotes = result.voteTotal;
+    const dVotes = result.ballotOptions.find(option => option.party.name[0].text === 'Democratic').voteCount;
+    return {
+        id,
+        percentage: dVotes / totalVotes,
+    };
+});
+
+fs.writeFileSync('gis/fairfax/parsed.json', JSON.stringify(parsedData, null, 2));
