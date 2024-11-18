2532783255662ec752c4579ba5728fd4917106b1
osistlk <osistlk@gmail.com>
2024-11-17 21:30:24 -0500

    Add delta calculation and output to delta.json for ratio comparisons

--- gis/parse.js
+++ gis/parse.js
@@ -22,3 +22,24 @@ const keys = Object.keys(results[0]);
 header = keys.join(',');
 const csv = results.map(row => keys.map(key => row[key]).join(',')).join('\n');
 fs.writeFileSync('parsed.csv', header + '\n' + csv);
+
+const oldRatios = JSON.parse(fs.readFileSync('../data/votes.json', 'utf8'));
+
+// each dataset is an array of objects with id and ratio, oldRatios id is precinct.id, newRatios id is just id
+// compare ratio field and console.log if they are different
+const different = results.filter((newRatio) => {
+    const oldRatio = oldRatios.find(ratio => ratio.precinct.id == newRatio.id);
+    return oldRatio?.ratio !== newRatio?.ratio;
+});
+
+const delta = different.map((newRatio) => {
+    const oldRatio = oldRatios.find(ratio => ratio.precinct.id == newRatio.id);
+    return {
+        id: newRatio.id,
+        oldRatio: oldRatio?.ratio,
+        newRatio: newRatio?.ratio,
+        delta: newRatio?.ratio - oldRatio?.ratio
+    };
+});
+
+fs.writeFileSync('delta.json', JSON.stringify(delta, null, 2));
