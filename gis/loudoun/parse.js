02a77301a081ebbc1fcf4f5bfd743e49c0802a74
jacquesdaytona <osistlk@gmail.com>
2024-11-19 17:38:26 -0500

    Add data parsing and CSV/JSON export for Loudoun election results

---
+++ gis/loudoun/parse.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const data = JSON.parse(fs.readFileSync('gis/loudoun/data.json', 'utf8'));
+
+const parsedData = data.breakdownResults.map((result) => {
+    const name = result.precinct.name[0].text;
+    const id = name.split('-')[0].trim();
+    const totalVotes = result.voteTotal;
+    const dVotes = result.ballotOptions.find(option => option.party.name[0].text === 'Democratic').voteCount;
+    return {
+        id,
+        percentage: dVotes / totalVotes,
+    };
+});
+
+fs.writeFileSync('gis/loudoun/parsed.json', JSON.stringify(parsedData, null, 2));
+
+const keys = Object.keys(parsedData[0]);
+header = keys.join(',');
+const csv = parsedData.map(row => keys.map(key => row[key]).join(',')).join('\n');
+fs.writeFileSync('gis/loudoun/parsed.csv', header + '\n' + csv);
