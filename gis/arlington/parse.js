a582153e3c24a87c5394c2344322d8bfc0f54ff2
jacquesdaytona <osistlk@gmail.com>
2024-11-20 15:30:36 -0500

    Update Arlington data parsing to calculate and export vote percentages

--- gis/arlington/parse.js
+++ gis/arlington/parse.js
@@ -1,45 +1,21 @@
 const fs = require('fs');
 
-const data = JSON.parse(fs.readFileSync('ballot-items.json', 'utf8'));
+const data = JSON.parse(fs.readFileSync('gis/arlington/data.json', 'utf8'));
 
-const subset = data.breakdownResults;
-
-const results = subset.map((item) => {
-    const name = item.precinct.name[0].text;
+const parsedData = data.breakdownResults.map((result) => {
+    const name = result.precinct.name[0].text;
     const id = name.split('-')[0].trim();
-    const dVotes = item.ballotOptions.find(option => option.party.name[0].text === 'Democratic').voteCount;
-    const rVotes = item.ballotOptions.find(option => option.party.name[0].text === 'Republican').voteCount;
-    const voteRatio = dVotes / rVotes;
+    const totalVotes = result.voteTotal;
+    const dVotes = result.ballotOptions.find(option => option.party.name[0].text === 'Democratic').voteCount;
     return {
         id,
-        ratio: voteRatio
+        percentage: dVotes / totalVotes,
     };
 });
 
-fs.writeFileSync('parsed.json', JSON.stringify(results, null, 2));
+fs.writeFileSync('gis/arlington/parsed.json', JSON.stringify(parsedData, null, 2));
 
-const keys = Object.keys(results[0]);
+const keys = Object.keys(parsedData[0]);
 header = keys.join(',');
-const csv = results.map(row => keys.map(key => row[key]).join(',')).join('\n');
-fs.writeFileSync('parsed.csv', header + '\n' + csv);
-
-const oldRatios = JSON.parse(fs.readFileSync('../data/votes.json', 'utf8'));
-
-// each dataset is an array of objects with id and ratio, oldRatios id is precinct.id, newRatios id is just id
-// compare ratio field and console.log if they are different
-const different = results.filter((newRatio) => {
-    const oldRatio = oldRatios.find(ratio => ratio.precinct.id == newRatio.id);
-    return oldRatio?.ratio !== newRatio?.ratio;
-});
-
-const delta = different.map((newRatio) => {
-    const oldRatio = oldRatios.find(ratio => ratio.precinct.id == newRatio.id);
-    return {
-        id: newRatio.id,
-        oldRatio: oldRatio?.ratio,
-        newRatio: newRatio?.ratio,
-        delta: newRatio?.ratio - oldRatio?.ratio
-    };
-});
-
-fs.writeFileSync('delta.json', JSON.stringify(delta, null, 2));
+const csv = parsedData.map(row => keys.map(key => row[key]).join(',')).join('\n');
+fs.writeFileSync('gis/arlington/parsed.csv', header + '\n' + csv);
